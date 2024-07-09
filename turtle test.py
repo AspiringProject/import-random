dc5cfa454a86fbfc0265b1222137779e42241776
Oliver de Nijs <sth20denijso@sth.school>
2024-07-09 10:05:31 +0100

    Add files via upload

---
+++ turtle test.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.bgcolor("orange")
+turtle.pensize(1)
+x = 0
+while x < 1:
+    for i in range(0,8):
+        turtle.right(45)
+        turtle.forward(200)
+    turtle.right(1)
+
+turtle.exitonclick()
---
+++ turtle.py
@@ -0,0 +1,6 @@
+import turtle
+
+t.right(90)
+t.forward(100)
+t.left(90)
+t.backward(100)
