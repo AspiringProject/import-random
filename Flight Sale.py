5486454c78f2dfd6972e7b31d36090c7b8203a78
Oliver de Nijs <sth20denijso@sth.school>
2024-07-16 09:11:30 +0000

    done.

--- Flight Sale.py
+++ Flight Sale.py
@@ -1,7 +1,7 @@
-clas = input("What class are you flying? (Standard or First): ")
+clas = input("What class are you flying? (standard or first): ")
 
 def aes(standard,distance):
-    if clas == "Standard":
+    if clas.lower() == "standard":
         if distance >1000:
             standard = standard*0.85
             return standard
@@ -10,14 +10,14 @@ def aes(standard,distance):
             return standard
 
 def sha(standard,first,distance):
-    if clas == "Standard":
+    if clas.lower() == "standard":
         if distance >1000:
             standard = standard*0.85
             return standard
         else:
             standard = standard*0.925
             return standard
-    elif clas == "First":
+    elif clas.lower() == "first":
         if distance >1000:
             first = first*0.70
             return first
@@ -26,14 +26,14 @@ def sha(standard,first,distance):
             return first
 
 def otp(standard,first,distance):
-    if clas == "Standard":
+    if clas.lower() == "standard":
         if distance >1000:
             standard = standard*0.85
             return standard
         else:
             standard = standard*0.925
             return standard
-    elif clas == "First":
+    elif clas.lower() == "first":
         if distance >1000:
             first = first*0.70
             return first
@@ -42,14 +42,14 @@ def otp(standard,first,distance):
             return first
 
 def tls(standard,first,distance):
-    if clas == "Standard":
+    if clas.lower() == "standard":
         if distance >1000:
             standard = standard*0.85
             return standard
         else:
             standard = standard*0.925
             return standard
-    elif clas == "First":
+    elif clas.lower() == "first":
         if distance >1000:
             first = first*0.70
             return first
@@ -58,12 +58,11 @@ def tls(standard,first,distance):
             return first
 
 flight = input("Where is the destination? (3 letters): ")
-if flight == "aes":
+if flight.lower() == "aes":
     print("The cost of your ticket is £",aes(106,700))
-elif flight == "sha":
+elif flight.lower() == "sha":
     print("The cost of your ticket is £",sha(1000,2200,5700))
-elif flight == "otp":
+elif flight.lower() == "otp":
     print("The cost of your ticket is £",otp(95,190,1400))
-elif flight == "tls":
+elif flight.lower() == "tls":
     print("The cost of your ticket is £",tls(115,210,690))
-    
